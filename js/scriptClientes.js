20c36479d144415cad46efb5aba00cebced50ff6
Jesus Moreno <jmoreno91@alumnos.uaq.mx>
2023-06-12 09:40:18 -0600

    Se terminaron los scripts de clientes y proveedores

--- js/scriptClientes.js
+++ js/scriptClientes.js
@@ -3,7 +3,16 @@ window.onload=init;
 
 function init()
 {
-    mostrarDetallesVenta()
+    mostrarClientes();
+
+    const searchInput=document.getElementById("BuscarCliente");
+    searchInput.addEventListener("keydown",function(event)
+    {
+        if(event.key =="Enter")
+        {
+            buscarCliente();
+        }
+    });
 }
 
 const venta=[
@@ -12,27 +21,81 @@ const venta=[
         Nombre:"Pancracio",
         Apellido:"López",
         Direccion:"Ronda Ander,6, Entre suelo 6°",
+        Correo:"pancracio@email.com",
         Telefono:"442-138-68-82",
         VentaReciente: {
             idV:1
         }
+    },
+    {
+        id:2,
+        Nombre:"Saul",
+        Apellido:"Hernández",
+        Direccion:"Ronda Ander,6, Entre suelo 6°",
+        Correo:"eldejaguares@email.com",
+        Telefono:"447-138-67-82",
+        VentaReciente: {
+            idV:2
+        }
     }
 ];
 
+//función para mostrar los detalles de los clientes 
 function mostrarDetallesVenta(venta)
 {
     const detallesVentaHtml=`
     <div id="DetalleClientes">
-        <h2>Id:<br> <span>${venta.id}</span></h2>
+        <h2>Id: <span>${venta.id}</span></h2>
         <p><strong>Nombre: </strong><span>${venta.Nombre}</span></p>
         <p><strong>Apellido: </strong><span>${venta.Apellido}</span></p>
-        <p><strong>Dirección: </strong></p>
-        <p><strong>Correo: </strong></p>
-        <p><strong>Teléfono: </strong></p>
+        <p><strong>Dirección: </strong><span>${venta.Direccion}</span></p>
+        <p><strong>Correo: </strong><span>${venta.Correo}</span></p>
+        <p><strong>Teléfono: </strong><span>${venta.Telefono}</span></p>
     </div>
     <div id="VentaReciente">
         <h2>Ventas recientes</h2>
-        <p>00000000001</p>
-    </div>`
+        <p><span>${venta.VentaReciente.idV}</span></p>
+    </div>`;
+    const ventasContainer=document.getElementById("ClientesContainer");
+    ventasContainer.innerHTML=detallesVentaHtml;
+}
+
+//Funcion para mostrar todos los clientes
+function mostrarClientes()
+{
+    const ventasContainer=document.getElementById("ClientesContainer");
+    ventasContainer.innerHTML="";
+    for(const clienteItem of venta)
+    {
+        const detalleClienteHtml=`
+        <div id="DetalleClientes">
+            <h2>Id:<br> <span>${clienteItem.id}</span></h2>
+            <p><strong>Nombre: </strong><span>${clienteItem.Nombre}</span></p>
+            <p><strong>Apellido: </strong><span>${clienteItem.Apellido}</span></p>
+            <p><strong>Dirección: </strong><span>${clienteItem.Direccion}</span></p>
+            <p><strong>Correo: </strong><span>${clienteItem.Correo}</span></p>
+            <p><strong>Teléfono: </strong><span>${clienteItem.Telefono}</span></p>
+         </div>
+        <div id="VentaReciente">
+            <h2>Ventas recientes</h2>
+            <p><span>${clienteItem.VentaReciente.idV}</span></p>
+        </div>`;
+        ventasContainer.innerHTML += detalleClienteHtml;
+    }
 }
     
+
+function buscarCliente()
+{
+    const searchInput =document.getElementById("BuscarCliente").value;
+    const clienteEncontrado=venta.find(venta => venta.id == searchInput);
+
+    if(clienteEncontrado)
+    {
+        mostrarDetallesVenta(clienteEncontrado);
+    }
+    else
+    {
+        alert("No se encontró ningún cliente con ese id");
+    }
+}
---
+++ js/scriptProveedores.js
@@ -0,0 +1,95 @@
+window.onload=init;
+
+function init()
+{
+    mostrarProveedores();
+
+    const searchInput=document.getElementById("BuscarProveedor");
+    searchInput.addEventListener("keydown",function(event)
+    {
+        if(event.key == "Enter")
+        {
+            buscarProveedor();
+        }
+    })
+}
+
+const Proveedor=[
+    {
+        id:1,
+        Nombre:"Pizza Napolitana",
+        Direccion:"Ronda Ander,6, Entre suelo 6°",
+        Correo:"pizzanapolitana@email.com",
+        Telefono:"001-138-68-82",
+        CompraReciente:{
+            idCompra:1
+        }
+    },
+    {
+        id:2,
+        Nombre:"Coca-Cola",
+        Direccion:"Ronda Ander,6, Entre suelo 6°",
+        Correo:"cocacola@email.com",
+        Telefono:"001-138-68-82",
+        CompraReciente:{
+            idCompra:2
+        }
+    }
+]
+
+//Función para mostrar los detalles de los clientes 
+function mostrarDetallesProveedor(Proveedor)
+{
+    const DetallesProveedor=`
+    <div id="DetallesProveedor">
+        <h2>Id: <span>${Proveedor.id}</span></h2>
+        <p><strong>Nombre: </strong><span>${Proveedor.Nombre}</span></p>
+        <p><strong>Dirección: </strong><span>${Proveedor.Direccion}</span></p>
+        <p><strong>Correo: </strong><span>${Proveedor.Correo}</span></p>
+        <p><strong>Teléfono: </strong><span>${Proveedor.Telefono}<span></p>
+    </div>
+    <div id="CompraReciente">
+        <h2>Compras recientes</h2>
+        <p><span>${Proveedor.CompraReciente.idCompra}</p>
+    </div>`;
+    const ProveedorContainer=document.getElementById("ProveedorContainer");
+    ProveedorContainer.innerHTML=DetallesProveedor;
+}
+
+function mostrarProveedores()
+{
+    const ProveedorContainer=document.getElementById("ProveedorContainer");
+    ProveedorContainer.innerHTML="";
+    for(const proveedorItem of Proveedor)
+    {
+        const detelleProveedorHtml=`
+        <div id="DetallesProveedor">
+            <h2>Id: <span>${proveedorItem.id}</span></h2>
+            <p><strong>Nombre: </strong><span>${proveedorItem.Nombre}</span></p>
+            <p><strong>Dirección: </strong><span>${proveedorItem.Direccion}</span></p>
+            <p><strong>Correo: </strong><span>${proveedorItem.Correo}</span></p>
+            <p><strong>Teléfono: </strong><span>${proveedorItem.Telefono}<span></p>
+        </div>
+        <div id="CompraReciente">
+            <h2>Compras recientes</h2>
+            <p><span>${proveedorItem.CompraReciente.idCompra}</p>
+        </div>`;
+        ProveedorContainer.innerHTML += detelleProveedorHtml;
+    }
+}
+
+
+function buscarProveedor()
+{
+    const searchInput =document.getElementById("BuscarProveedor").value;
+    const proveedorEncontrado=Proveedor.find(Proveedor => Proveedor.id == searchInput);
+
+    if(proveedorEncontrado)
+    {
+        mostrarDetallesProveedor(proveedorEncontrado);
+    }
+    else
+    {
+        alert("No se encontró ningun proveedor con ese id");
+    }
+}
