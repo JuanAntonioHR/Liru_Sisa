89fc73c72fe9104f99deb0724dddfb6abc0190fe
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-06-11 21:35:54 -0600

    A medio corregir scriptCompras e Historial_Compras
    Lo subo porque lo voy a borrar todo y que tal si explota

--- scriptCompras.js
+++ scriptCompras.js
@@ -1,31 +1,53 @@
+window.onload = init;
+
+function init() {
+  // if(!localStorage.getItem('token')) {
+  //   mostrarDetallesCompra(1);
+  // } else {
+  //   window.location.href = '#';
+  // }
+  mostrarDetallesCompra(compra[0]);
+}
+
 // Datos de ejemplo
-const compra = {
-  id: 1,
-  fecha: new Date(2023,3,23,11,29,0),
-  total: 530250.00,
-  estado: "Pagado, Entregado",
-  articulo: {
-    nombre: "Harina para pizza marca pizza Napolitana",
-    cantidad: 101
+const compra = [
+  {
+    id: 1,
+    fecha: new Date(2023, 3, 23, 11, 29, 0),
+    total: 530250.00,
+    estado: "Pagado, Entregado",
+    articulo: {
+      nombre: "Harina para pizza marca pizza Napolitana",
+      cantidad: 101
+    },
+    direccionEnvio: {
+      origen: "Av. Constituyentes Ote. 7-1, Hidalgo, Local D 76046, Coloniam San Francisquito, 76040 Santiago de Queretaro, Qro",
+      destino: "Praza de Jesus, 6, 47-B"
+    }
   },
-  direccionEnvio: {
-    origen: "Av. Constituyentes Ote. 7-1, Hidalgo, Local D 76046, Coloniam San Francisquito, 76040 Santiago de Queretaro, Qro",
-    destino: "Praza de Jesus, 6, 47-B"
+  {
+    id: 2,
+    fecha: new Date(2023, 5, 11, 14, 45, 0),
+    total: 980.00,
+    estado: "Pagado, Pendiente de entrega",
+    articulo: {
+      nombre: "Leche descremada",
+      cantidad: 4
+    },
+    direccionEnvio: {
+      origen: "Calle del Sol, 123",
+      destino: "Avenida de la Luna, 456"
+    }
   }
-};
+];
 
 // Función para mostrar los detalles de la compra
-function mostrarDetallesCompra() {
+function mostrarDetallesCompra(compra) {
   document.getElementById("id").textContent = compra.id;
   document.getElementById("fecha").textContent = compra.fecha;
   document.getElementById("total").textContent = "$" + compra.total;
   document.getElementById("estado").textContent = compra.estado;
-
-  const listaArticulo = document.getElementById("listaArticulo");
-  const item = document.createElement("li");
-  item.textContent = compra.articulo.nombre + " x" + compra.articulo.cantidad;
-  listaArticulo.appendChild(item);
-
+  document.getElementById("listaArticulo").textContent = compra.articulo.nombre + " x" + compra.articulo.cantidad;
   document.getElementById("origen").textContent = compra.direccionEnvio.origen;
   document.getElementById("destino").textContent = compra.direccionEnvio.destino;
 }
@@ -33,9 +55,12 @@ function mostrarDetallesCompra() {
 // Función para buscar un producto por ID
 function buscarProducto() {
   const searchInput = document.getElementById("searchInput").value;
-  if (searchInput == compra.id) {
-    mostrarDetallesCompra();
+  const productoEncontrado = compra.find(producto => producto.id == searchInput);
+
+  if (productoEncontrado) {
+    mostrarDetallesCompra(productoEncontrado);
   } else {
-    alert("No se encontró ningun producto con ese ID.");
+    alert("No se encontró ningún producto con ese ID.");
   }
 }
+
