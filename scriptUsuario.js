320bd26f112282ecbd52d879f71f5ce37414168a
Ian Isaak <107010373+IanIsaak@users.noreply.github.com>
2023-06-11 22:52:08 -0600

    Perfil de usuario y su script
    Base del perfil de usuario con su script, como tal el boton todavia no funciona ya que al recargar la pagina no se guardan los datos cambiados y tambien falta agregar las ventas recientes del usuario

---
+++ scriptUsuario.js
@@ -0,0 +1,8 @@
+function actualizarDatos() {
+  var correo = document.getElementById("correo").value;
+  var telefono = document.getElementById("telefono").value;
+
+  // Aquí puedes realizar la lógica para actualizar los datos en tu sistema
+
+  document.getElementById("mensaje").innerText = "Datos actualizados";
+}
