0b43fc823475ea8fe1dcfbbd4d4237c57e5a5332
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-06-14 03:27:01 -0600

    css de menu de inicio implementado

---
+++ js/scriptLogin.js
@@ -0,0 +1,15 @@
+document.querySelector('.Boton-inicio').addEventListener('click', function() {
+    var usuarioInput = document.querySelector('.Usuario');
+    var contraseñaInput = document.querySelector('.Contraseña');
+    
+    var usuario = usuarioInput.value;
+    var contraseña = contraseñaInput.value;
+    
+    if (usuario === '000001' && contraseña === '12345678') {
+      var token = 'GENERAR_AQUÍ_UN_TOKEN_ALEATORIO';
+      localStorage.setItem('token', token);
+      window.location.href = 'Menu_Inicio.html';
+    } else {
+      alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
+    }
+});
