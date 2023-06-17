39bb12856f34971a3d15a9383151b0fabb734ed4
Ian Isaak <107010373+IanIsaak@users.noreply.github.com>
2023-06-16 20:04:53 -0600

    Usuario tanto js como html actualizado
    Se pueden actualizar los datos, aparece ventana de datos actualizados y ya aparecen las ventas con el id y fecha-hora

--- scriptUsuario.js
+++ scriptUsuario.js
@@ -1,8 +1,91 @@
+window.onload = init;
+
+function init() {
+  if(localStorage.getItem('token') == 1) {
+    MostrarVentas();
+  
+    const searchInput = document.getElementById("searchInput");
+    searchInput.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        buscarProducto();
+      }
+    });
+  } else {
+    window.location.href = 'InicioSesion.html';
+  }
+}
+
+function logout() {
+  localStorage.removeItem('token');
+  window.location.href = 'InicioSesion.html';
+}
+
 function actualizarDatos() {
   var correo = document.getElementById("correo").value;
   var telefono = document.getElementById("telefono").value;
 
-  // Aquí puedes realizar la lógica para actualizar los datos en tu sistema
+  //Guarda los datos en el localStorage
+  localStorage.setItem("correo", correo);
+  localStorage.setItem("telefono", telefono);
 
-  document.getElementById("mensaje").innerText = "Datos actualizados";
+  alert('Datos modificados.');
 }
+
+//Obtiene los datos guardados en el localStorage al recargar la pagina
+window.addEventListener("load", function() {
+  var correoGuardado = localStorage.getItem("correo");
+  var telefonoGuardado = localStorage.getItem("telefono");
+
+  //Verifica si hay datos guardados y actualizar los campos correspondientes
+  if (correoGuardado && telefonoGuardado) {
+    document.getElementById("correo").value = correoGuardado;
+    document.getElementById("telefono").value = telefonoGuardado;
+  }
+});
+
+const Venta=[
+    {
+        Id:1,
+        Dia:"16-04-23",
+        Hora:"11:29 a.m",
+    },
+    {
+        Id:2,
+        Dia:"16-04-23",
+        Hora:"11:40 a.m",
+    }
+    
+]
+
+function MostrarDetallesVenta(Venta)
+{
+    const DetallesVenta=`
+    <div class="ventas-card-item">
+        <div id="DetallesVenta">
+            <h2>Id: <span>${Venta.Id}</span></h2>
+            <p><strong>Día y Hora: </strong><span>${Venta.Dia}, ${Venta.Hora}</span></p>
+        </div>
+    </div>`;
+    const VentaContainer=document.getElementById("ventaContainer");
+    VentaContainer.innerHTML=DetallesVenta;
+}
+
+function MostrarVentas()
+{
+    const VentaContainer=document.getElementById("ventaContainer");
+    VentaContainer.innerHTML="";
+    for(const ventaItem of Venta)
+    {
+        const detalleVentaHtml=`
+        <div class="ventas-card-item">
+            <div id="DetallesVenta">
+                <h2>Id: <span>${ventaItem.Id}</span></h2>
+                <p><strong>Día y Hora: </strong><span>${ventaItem.Dia}, ${ventaItem.Hora}</span></p>
+            </div>  
+        </div>`;
+        VentaContainer.innerHTML += detalleVentaHtml;
+    }
+   
+}
+
+
